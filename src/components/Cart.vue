59a0a36ae1552f4f4efaa8ae2d1fd1469a442296
Dima-lang51 <68560471+Dima-lang51@users.noreply.github.com>
2022-05-01 18:07:08 +0300

    Add files via upload

---
+++ src/components/Cart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cart-list">
+    <div v-for="item in spisok_dlya_rendera">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cart: Array,
+  },
+  computed: {
+      spisok_dlya_rendera() {
+        let arrayReverse = [...this.cart].reverse();
+
+          return arrayReverse;
+      },
+  },
+};
+</script>
+
+<style>
+
+</style>
